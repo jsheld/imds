d1897683e8c29244a1fc63f94c847f1b5b4d853f
jsheld <jsheld@hotmail.com>
2023-08-17 23:37:40 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ from flask import Flask
 import requests
 
 
-r = requests.put('http://169.254.169.254/latest/api/token', headers={'X-aws-ec2-metadata-token-ttl-seconds': '21600'})
+r = requests.put('http://169.254.169.254/latest/api/token', headers={'X-aws-ec2-metadata-token-ttl-seconds': '21600',
+  'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'}
 token = r.content
 
 PORT = 8080
