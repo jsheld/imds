e9d5c3fd30491e16b163a3adc650dc1d7b230771
jsheld <jsheld@hotmail.com>
2023-08-17 22:53:32 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,17 @@
 from flask import Flask
-import subprocess
-import os
+import requests
+
+
+r = requests.put('http://169.254.169.254/latest/api/token', headers={'X-aws-ec2-metadata-token-ttl-seconds': '21600'})
+token = r.content
 
 PORT = 8080
-
-cmd = "TOKEN=`curl -X PUT \"http://169.254.169.254/latest/api/token\" -H \"X-aws-ec2-metadata-token-ttl-seconds: 21600\"` \
-      && curl -H \"X-aws-ec2-metadata-token: $TOKEN\" -v http://169.254.169.254/latest/meta-data/"
-
-try:
-    proc = subprocess.Popen([cmd], stdout=subprocess.PIPE, shell=True)
-    (message, err) = proc.communicate()
-except Exception as e:
-   message = err;
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-  return "<html><head><title/><body>" + str(message) + "</body></html>"
+  return token
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=PORT)
